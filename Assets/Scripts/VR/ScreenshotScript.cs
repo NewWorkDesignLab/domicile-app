b2e01eecf288eee6123a866b10210be1f883d498
Tobias Bohn <info@tobiasbohn.com>
2020-11-23 19:31:13 +0100

    photos manageable on andoid gallery halfway fixed

--- Assets/Scripts/VR/ScreenshotScript.cs
+++ Assets/Scripts/VR/ScreenshotScript.cs
@@ -1,117 +1,85 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 #if PLATFORM_ANDROID
 using UnityEngine.Android;
 #endif
 
-public class ScreenshotScript : MonoBehaviour
-{
+public class ScreenshotScript : MonoBehaviour {
   public static ScreenshotScript instance;
   bool isBusy = false;
-  void Awake()
-  {
+
+  void Awake () {
     instance = this;
   }
 
-  // private bool isSingleTap = false;
-  // private float lastTap;
-  // private float doubleTapOffset = .5f;
-
-  // void Start()
-  // {
-  //   lastTap = Time.realtimeSinceStartup;
-  // }
-
-  // void Update()
-  // {
-  //   if (Input.touchCount > 0 || Input.GetKeyDown("space") || Input.GetMouseButtonDown(0))
-  //   {
-  //     if (!isSingleTap)
-  //     {
-  //       isSingleTap = true;
-  //       StartCoroutine(WaitForSecondTap());
-  //     }
-  //     else if ((Time.realtimeSinceStartup - lastTap) < doubleTapOffset)
-  //     {
-  //       // IS DOUBLE TAP
-  //       isSingleTap = false;
-  //       Debug.Log("DOUBLE TAP");
-  //       EndScenario.instance.InvokeEndScenario();
-  //     }
-  //     lastTap = Time.realtimeSinceStartup;
-  //   }
-  // }
-
-  // private IEnumerator WaitForSecondTap()
-  // {
-  //   yield return new WaitForSecondsRealtime(doubleTapOffset);
-  //   if (isSingleTap)
-  //   {
-  //     // IS SINGLE TAP
-  //     TakeScreenshot();
-  //     Debug.Log("SINGLE TAP");
-  //     isSingleTap = false;
-  //   }
-  // }
-
-  // public void TakeScreenshot()
-  // {
-  //   string time = System.DateTime.Now.ToString("yyyyMMddHHmmssfff");
-  //   string name = string.Format("{0}/domicile_{1}.png", Application.persistentDataPath, time);
-  //   ScreenCapture.CaptureScreenshot(name);
-  //   CrossSceneManager.instance.unsavedScreenshots = CrossSceneManager.instance.unsavedScreenshots == null ? new List<string>() : CrossSceneManager.instance.unsavedScreenshots;
-  //   CrossSceneManager.instance.unsavedScreenshots.Add(name);
-  // }
-
-  public void TakeScreenshot(Action callback)
-  {
-#if PLATFORM_ANDROID
-        if (!Permission.HasUserAuthorizedPermission(Permission.ExternalStorageRead))
-        {
-            Permission.RequestUserPermission(Permission.ExternalStorageRead);
-        }
-        if (!Permission.HasUserAuthorizedPermission(Permission.ExternalStorageWrite))
-        {
-            Permission.RequestUserPermission(Permission.ExternalStorageWrite);
-        }
-#endif
-    if (!isBusy)
-    {
+  public void TakeScreenshot (Action callback) {
+    if (!isBusy) {
       isBusy = true;
-      var notification = new List<string>();
-      notification.Add("Screenshot in 3");
-      notification.Add("Screenshot in 2");
-      notification.Add("Screenshot in 1");
-      HUD.instance.ShowNotification(notification, 1f, () =>
-          {
-            StartCoroutine(SaveScreenshot(() =>
-            {
-              HUD.instance.ShowNotification("Screenshot gespeichert", 1f);
-              isBusy = false;
-              callback();
-            }));
-          });
-    }
-    else
-    {
-      Debug.LogWarning("[ScreenshotScript TakeScreenshot] Cannot take Screenshot. Is Busy.");
+      var notification = new List<string> ();
+      notification.Add ("Screenshot in 2");
+      notification.Add ("Screenshot in 1");
+      HUD.instance.ShowNotification (notification, 1f, () => {
+        StartCoroutine (SavePhoto (() => {
+          HUD.instance.ShowNotification ("Screenshot gespeichert", 1f);
+          isBusy = false;
+          callback ();
+        }));
+      });
+    } else {
+      Debug.LogWarning ("[ScreenshotScript TakeScreenshot] Cannot take Screenshot. Is Busy.");
     }
   }
 
-  IEnumerator SaveScreenshot(Action callback)
-  {
-    yield return new WaitForEndOfFrame();
+  public IEnumerator SavePhoto (Action callback) {
+#if PLATFORM_ANDROID
+    Texture2D texture = new Texture2D (Screen.width, Screen.height, TextureFormat.RGB24, false);
+    texture.ReadPixels (new Rect (0, 0, Screen.width, Screen.height), 0, 0, false);
+    texture.Apply ();
+    string myFileName = "screenshot_domicile_" + System.DateTime.Now.Hour + System.DateTime.Now.Minute + System.DateTime.Now.Second;
+    var imageUrl = SaveImageToGallery (texture, myFileName, "Saving photo to android");
+    var imageUrl_2 = imageUrl.Replace("content:/", "");
+    Debug.Log(imageUrl);
+    Debug.Log(imageUrl_2);
 
-    string myFileName = "screenshot_domicile_" + System.DateTime.Now.Hour + System.DateTime.Now.Minute + System.DateTime.Now.Second + ".png";
-    string myDefaultLocation = Application.persistentDataPath + "/" + myFileName;
+    yield return new WaitForEndOfFrame ();
+    CrossSceneManager.instance.unsavedScreenshots = CrossSceneManager.instance.unsavedScreenshots == null ? new List<string> () : CrossSceneManager.instance.unsavedScreenshots;
+    CrossSceneManager.instance.unsavedScreenshots.Add (imageUrl_2);
+#else
+    Debug.Log ("Would Save Screenshot only on Android");
+#endif
+    callback ();
+  }
 
-    ScreenCapture.CaptureScreenshot(myFileName);
-    yield return new WaitForSeconds(1);
+  // following Screenshot / Java / Android Code copied from:
+  // https://answers.unity.com/questions/204372/saving-screenshots-to-android-gallery.html?_ga=2.33230142.1821600655.1606136243-1256100704.1580052593
+  private const string MediaStoreImagesMediaClass = "android.provider.MediaStore$Images$Media";
+  public static string SaveImageToGallery (Texture2D texture2D, string title, string description) {
+    using (var mediaClass = new AndroidJavaClass (MediaStoreImagesMediaClass)) {
+      using (var cr = Activity.Call<AndroidJavaObject> ("getContentResolver")) {
+        var image = Texture2DToAndroidBitmap (texture2D);
+        var imageUrl = mediaClass.CallStatic<string> ("insertImage", cr, image, title, description);
+        return imageUrl;
+      }
+    }
+  }
 
-    CrossSceneManager.instance.unsavedScreenshots = CrossSceneManager.instance.unsavedScreenshots == null ? new List<string>() : CrossSceneManager.instance.unsavedScreenshots;
-    CrossSceneManager.instance.unsavedScreenshots.Add(myDefaultLocation);
-    callback();
+  public static AndroidJavaObject Texture2DToAndroidBitmap (Texture2D texture2D) {
+    byte[] encoded = texture2D.EncodeToPNG ();
+    using (var bf = new AndroidJavaClass ("android.graphics.BitmapFactory")) {
+      return bf.CallStatic<AndroidJavaObject> ("decodeByteArray", encoded, 0, encoded.Length);
+    }
+  }
+
+  private static AndroidJavaObject _activity;
+  public static AndroidJavaObject Activity {
+    get {
+      if (_activity == null) {
+        var unityPlayer = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
+        _activity = unityPlayer.GetStatic<AndroidJavaObject> ("currentActivity");
+      }
+      return _activity;
+    }
   }
 }
