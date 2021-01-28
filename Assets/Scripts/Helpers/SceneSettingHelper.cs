dc7c41fa0ca18c2daa8b06d47d38f9cb5317ce99
Tobias Bohn <info@tobiasbohn.com>
2021-01-28 23:12:28 +0100

    adjusted scene setting helper and changes in xr management

--- Assets/Scripts/Helpers/SceneSettingHelper.cs
+++ Assets/Scripts/Helpers/SceneSettingHelper.cs
@@ -1,47 +1,80 @@
 using System;
 using System.Collections;
 using UnityEngine;
-using UnityEngine.XR;
+using UnityEngine.XR.Management;
 
 public class SceneSettingHelper : MonoBehaviour {
-    public ScreenOrientation orientation;
+    // public ScreenOrientation orientation;
+    // public VRSetting vrSetting;
+
+    // void Start () {
+    //     Screen.orientation = orientation;
+    //     if (vrSetting == VRSetting.virtualReality) {
+    //         StartCoroutine (SwitchToVR ());
+    //     } else {
+    //         StartCoroutine (SwitchTo2D ());
+    //     }
+    // }
+
+    // private IEnumerator SwitchToVR () {
+    //     // copied from https://developers.google.com/vr/develop/unity/guides/hybrid-apps
+    //     string desiredDevice = "cardboard";
+    //     if (String.Compare (XRSettings.loadedDeviceName, desiredDevice, true) != 0) {
+    //         XRSettings.LoadDeviceByName (desiredDevice);
+    //         yield return null;
+    //     }
+    //     XRSettings.enabled = true;
+    // }
+
+    // IEnumerator SwitchTo2D () {
+    //     XRSettings.LoadDeviceByName ("");
+    //     yield return null;
+    //     ResetCameras ();
+    // }
+
+    // // Resets camera transform and settings on all enabled eye cameras.
+    // void ResetCameras () {
+    //     for (int i = 0; i < Camera.allCameras.Length; i++) {
+    //         Camera cam = Camera.allCameras[i];
+    //         if (cam.enabled && cam.stereoTargetEye != StereoTargetEyeMask.None) {
+    //             cam.transform.localPosition = Vector3.zero;
+    //             cam.transform.localRotation = Quaternion.identity;
+    //         }
+    //     }
+    // }
     public VRSetting vrSetting;
 
     void Start () {
-        Screen.orientation = orientation;
-        if (vrSetting == VRSetting.virtualReality) {
+        if (vrSetting == VRSetting.VR) {
             StartCoroutine (SwitchToVR ());
         } else {
-            StartCoroutine (SwitchTo2D ());
+            SwitchTo2D ();
         }
     }
 
     private IEnumerator SwitchToVR () {
-        // copied from https://developers.google.com/vr/develop/unity/guides/hybrid-apps
-        string desiredDevice = "cardboard";
-        if (String.Compare (XRSettings.loadedDeviceName, desiredDevice, true) != 0) {
-            XRSettings.LoadDeviceByName (desiredDevice);
-            yield return null;
-        }
-        XRSettings.enabled = true;
-    }
-
-    IEnumerator SwitchTo2D () {
-        XRSettings.LoadDeviceByName ("");
+        // from https://forum.unity.com/threads/toggle-between-2d-and-google-cardboard.902378/
+        Screen.orientation = ScreenOrientation.Landscape;
+#if UNITY_EDITOR
+        Debug.Log ("[SceneSettingHelper] Would Display Scene in Virtual Reality");
         yield return null;
-        ResetCameras ();
+#else
+        yield return XRGeneralSettings.Instance.Manager.InitializeLoader ();
+        XRGeneralSettings.Instance.Manager.StartSubsystems ();
+#endif
     }
 
-    // Resets camera transform and settings on all enabled eye cameras.
-    void ResetCameras () {
-        for (int i = 0; i < Camera.allCameras.Length; i++) {
-            Camera cam = Camera.allCameras[i];
-            if (cam.enabled && cam.stereoTargetEye != StereoTargetEyeMask.None) {
-                cam.transform.localPosition = Vector3.zero;
-                cam.transform.localRotation = Quaternion.identity;
-            }
+    private void SwitchTo2D () {
+#if UNITY_EDITOR
+        Debug.Log ("[SceneSettingHelper] Would Display Scene in Normal Mode");
+#else
+        if (XRGeneralSettings.Instance.Manager.isInitializationComplete) {
+            XRGeneralSettings.Instance.Manager.StopSubsystems ();
+            XRGeneralSettings.Instance.Manager.DeinitializeLoader ();
         }
+#endif
+        Screen.orientation = ScreenOrientation.Portrait;
     }
 }
 
-public enum VRSetting { normal, virtualReality }
+public enum VRSetting { Normal, VR }
