0ea44a2ff7ee2736e7e9e65c5d113b459b66454b
Tobias Bohn <info@tobiasbohn.com>
2021-02-08 14:25:12 +0100

    added small parts of documentation

--- Assets/Project/Scripts/Classes/Execution.cs
+++ Assets/Project/Scripts/Classes/Execution.cs
@@ -1,50 +1,50 @@
 using System;
 using UnityEngine;
 
+/// <summary>
+/// Public class for execution objects. The Execution Class also exists in the web application. An
+/// execution is a single run of a scenario and contains all the information like the corresponding
+/// participation id or date and time of execution.
+/// </summary>
 [Serializable]
 public class Execution {
+    /// <summary>ID of the Execution.</summary>
     public int id;
+    /// <summary>ID of the associated participation.</summary>
     public int participation_id;
+    /// <summary>Date and Time of the creation of this Execution.</summary>
     public DateTime created_at;
+    /// <summary>Date and Time of the last modification of the Execution.</summary>
     public DateTime updated_at;
 
-    public static void Create (int participation_id, Action<Execution> onSuccess) {
-        Create (participation_id, onSuccess, () => { });
-    }
+    /// <summary>
+    /// Generates an API call to the web server, which creates an execution.
+    /// </summary>
+    /// <param name="participation_id">Required. The ID of the Participation which gets executed.</param>
+    /// <param name="onSuccess">Required. Callback for a successfull operation. Returns the created Execution.</param>
+    /// <param name="onFailure">Required. Callback for a unsuccessfull operation.</param>
     public static void Create (int participation_id, Action<Execution> onSuccess, Action onFailure) {
-        var data = new ExecutionCreateData (participation_id);
-        string json = JsonUtility.ToJson (data);
+        // Use double curly braces ({{; }}) to escape normal braces in String.Format
+        string json = String.Format ("{{\"execution\":{{\"participation_id\":{0}}}}}", participation_id);
         ServerManager.PostRequest ("/api/executions", json, (body) => {
+            // successfull server request
             Execution execution = JsonUtility.FromJson<Execution> (body);
             onSuccess (execution);
         }, onFailure);
     }
 
-    public static void UploadImages (int execution_id, string[] images, Action<Execution> onSuccess) {
-        UploadImages (execution_id, images, onSuccess, () => { });
-    }
+    /// <summary>
+    /// Uploads images of an execution to the web server.
+    /// </summary>
+    /// <param name="execution_id">Required. ID of the execution to which the images should be uploaded.</param>
+    /// <param name="images"Required. Array of image paths to be uploaded.></param>
+    /// <param name="onSuccess">Required. Callback for a successfull operation. Returns the created Execution.</param>
+    /// <param name="onFailure">Required. Callback for a unsuccessfull operation.</param>
     public static void UploadImages (int execution_id, string[] images, Action<Execution> onSuccess, Action onFailure) {
         ServerManager.ImageRequest (String.Format ("/api/executions/{0}/images", execution_id), images, (body) => {
+            // successfull server request
             Execution execution = JsonUtility.FromJson<Execution> (body);
             onSuccess (execution);
         }, onFailure);
-    }
-}
-
-[Serializable]
-public class ExecutionGroup {
-    public Execution[] executions;
-}
-
-[Serializable]
-class ExecutionCreateData {
-    public Execution execution = new Execution ();
-    public ExecutionCreateData (int participation_id) {
-        this.execution.participation_id = participation_id;
-    }
-
-    [Serializable]
-    public class Execution {
-        public int participation_id;
     }
 }
--- Assets/Project/Scripts/Classes/Participation.cs
+++ Assets/Project/Scripts/Classes/Participation.cs
@@ -1,62 +1,71 @@
 using System;
 using UnityEngine;
 
+/// <summary>
+/// Public class for participation objects. The Participation Class also exists in the web application.
+/// A participation is a single subscription to a scenario. A user may only participate in a scenario once.
+/// </summary>
 [Serializable]
 public class Participation {
+    /// <summary>ID of the Participation.</summary>
     public int id;
+    /// <summary>ID of the associated User.</summary>
     public int user_id;
+    /// <summary>ID of the associated Scenario.</summary>
     public int scenario_id;
+    /// <summary>Date and Time of the creation of this Participation.</summary>
     public DateTime created_at;
+    /// <summary>Date and Time of the last modification of the Participation.</summary>
     public DateTime updated_at;
 
-    public static void Index (Action<ParticipationGroup> onSuccess) {
-        Index (onSuccess, () => { });
-    }
+    /// <summary>
+    /// Generates an API query to retrieve a list of all participations of a User.
+    /// </summary>
+    /// <param name="onSuccess">Required. Callback for a successfull operation. Returns a Collection of Participations.</param>
+    /// <param name="onFailure">Required. Callback for a unsuccessfull operation.</param>
     public static void Index (Action<ParticipationGroup> onSuccess, Action onFailure) {
         ServerManager.GetRequest ("/api/participations", "", (body) => {
+            // successfull server request
             string editJson = "{\"participations\":" + body + "}";
             ParticipationGroup participations = JsonUtility.FromJson<ParticipationGroup> (editJson);
             onSuccess (participations);
         }, onFailure);
     }
 
-    public static void Show (int id, Action<Participation> onSuccess) {
-        Show (id, onSuccess, () => { });
-    }
+    /// <summary>
+    /// Generates an API query to retrieve information of a single Participation.
+    /// </summary>
+    /// <param name="id">Required. ID for which the Information should be queried.</param>
+    /// <param name="onSuccess">Required. Callback for a successfull operation. Returns a Participation.</param>
+    /// <param name="onFailure">Required. Callback for a unsuccessfull operation.</param>
     public static void Show (int id, Action<Participation> onSuccess, Action onFailure) {
         ServerManager.GetRequest (String.Format ("/api/participations/{0}", id), "", (body) => {
+            // successfull server request
             Participation participation = JsonUtility.FromJson<Participation> (body);
             onSuccess (participation);
         }, onFailure);
     }
 
-    public static void Create (int scenario_id, Action<Participation> onSuccess) {
-        Create (scenario_id, onSuccess, () => { });
-    }
+    /// <summary>
+    /// Generates an API call to the web server, which creates an Participation.
+    /// </summary>
+    /// <param name="scenario_id">Required. ID of the Scenario which the User will participate.</param>
+    /// <param name="onSuccess">Required. Callback for a successfull operation. Returns a Participation.</param>
+    /// <param name="onFailure">Required. Callback for a unsuccessfull operation.</param>
     public static void Create (int scenario_id, Action<Participation> onSuccess, Action onFailure) {
-        var data = new ParticipationCreateData (scenario_id);
-        string json = JsonUtility.ToJson (data);
+        string json = String.Format ("{{\"participation\":{{\"scenario_id\":{0}}}}}", scenario_id);
         ServerManager.PostRequest ("/api/participations", json, (body) => {
+            // successfull server request
             Participation participation = JsonUtility.FromJson<Participation> (body);
             onSuccess (participation);
         }, onFailure);
     }
 }
 
+/// <summary>
+/// Wrapper class for a collection of multiple Participations.
+/// </summary>
 [Serializable]
 public class ParticipationGroup {
     public Participation[] participations;
-}
-
-[Serializable]
-class ParticipationCreateData {
-    public Participation participation = new Participation ();
-    public ParticipationCreateData (int scenario_id) {
-        this.participation.scenario_id = scenario_id;
-    }
-
-    [Serializable]
-    public class Participation {
-        public int scenario_id;
-    }
 }
